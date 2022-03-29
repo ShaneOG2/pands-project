2f39a96a256743abeed7257cc2a49bff1c85079a
Shane O'G <G00411304@gmit.ie>
2022-03-29 12:37:14 +0100

    Sucessfully got dataset inputted correctly with collumn names added. Also wrote brief intro to dataset.

--- analysis.py
+++ analysis.py
@@ -4,13 +4,16 @@ import os
 import matplotlib as plt 
 import seaborn as sns
 
-df = pd.read_csv('iris.data')
-df.columns = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"]
+collumnNames = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"] # List of the collumn names to be added to dataframe
+df = pd.read_csv('iris.txt', names = collumnNames) # Reads in dataset as df and adds the collumn names to top
+
 
 print(df.head()) # Prints first 6 rows
 #print(df.info()) # Prints info about df
 #print(df.shape) # Prints shape of df
-print(df.describe()) # Prints a description of df 
-
+#print(df.describe()) # Prints a description of df 
+#
 #print(df.values) # Prints values of df 
 #print(df.columns) # Prints collumns of df
+
+
