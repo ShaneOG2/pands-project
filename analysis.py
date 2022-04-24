880760de7e310e5bf98eea92c21ee97480905667
Shane O'G <G00411304@gmit.ie>
2022-04-24 13:37:36 +0100

    Starting updating analysis.py with Machine Learning workings

--- analysis.py
+++ analysis.py
@@ -1,15 +1,38 @@
 # Author: Shane O'Gorman
 # Analysis of Iris Data set
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
-##### Importing data into dataframe
+def main():
+    ##### Importing data into dataframe
+    collumnNames = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"] # List of the collumn names to be added to dataframe
+    df = pd.read_csv('iris.txt', names = collumnNames) # Reads in dataset as df and adds the collumn names to top
 
-collumnNames = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"] # List of the collumn names to be added to dataframe
-df = pd.read_csv('iris.txt', names = collumnNames) # Reads in dataset as df and adds the collumn names to top
+    ##### Function calls
+    #summaryFile()
+    #plotBoxPlots()
+    #plotHistograms()
+    #pairPlots()
+    #plotScatterplots()
+    #corrMatrixPlot()
+
+    ##### Use df2 for machine learning work setting species to numerical values
+    df2 = df
+    from sklearn.preprocessing import LabelEncoder
+    le = LabelEncoder()
+    df2["Species"] = le.fit_transform(df2["Species"])
+
+    X = df2.drop(["Species"], axis=1)
+    Y = df2["Species"]
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
+
+    from sklearn.linear_model import LogisticRegression
+    logreg = LogisticRegression()
+    logreg.fit(x_train,y_train)
+    
+
 
 ##### Writes to summary file
 
@@ -50,6 +73,19 @@ def summaryFile():
 
         f.write(("Correlation Matrix:\n\n")+(str(corrMatrix)+('\n\n')))
 
+##### Plots Boxplots
+
+def plotBoxPlots():
+    sns.set(style="darkgrid")
+
+    fig, axs = plt.subplots(2, 2, figsize=(12, 12))
+
+    sns.boxplot(data=df, x="Species", y="SepalLengthCm", ax=axs[0, 0], palette="Set2")
+    sns.boxplot(data=df, x="Species", y="SepalWidthCm", ax=axs[0, 1], palette="Set2")
+    sns.boxplot(data=df, x="Species", y="PetalLengthCm", ax=axs[1, 0], palette="Set2")
+    sns.boxplot(data=df, x="Species", y="SepalWidthCm", ax=axs[1, 1], palette="Set2")
+    plt.show()
+
 ##### Plots Histograms
 
 def plotHistograms():
@@ -63,6 +99,14 @@ def plotHistograms():
     sns.histplot(data=df, x="PetalWidthCm", kde=True, ax=axs[1, 1], hue="Species", palette="Set2")
     plt.show()
     #plt.savefig()
+
+##### Plots Pairplots
+
+def pairPlots():
+    sns.pairplot(df, kind="scatter", hue="Species", markers=["o", "s", "D"], palette="Set2")
+    plt.show()
+
+##### Plots Scatterplots
 
 def plotScatterplots():
     sns.set(style="darkgrid")
@@ -89,20 +133,7 @@ def plotScatterplots():
     sns.lmplot(data=df, x="PetalLengthCm", y="SepalWidthCm", fit_reg=False, hue="Species", markers=["o", "s", "D"], palette="Set2")
     plt.show()
 
-def pairPlots():
-    sns.pairplot(df, kind="scatter", hue="Species", markers=["o", "s", "D"], palette="Set2")
-    plt.show()
-
-def plotBoxPlots():
-    sns.set(style="darkgrid")
-
-    fig, axs = plt.subplots(2, 2, figsize=(12, 12))
-
-    sns.boxplot(data=df, x="Species", y="SepalLengthCm", ax=axs[0, 0], palette="Set2")
-    sns.boxplot(data=df, x="Species", y="SepalWidthCm", ax=axs[0, 1], palette="Set2")
-    sns.boxplot(data=df, x="Species", y="PetalLengthCm", ax=axs[1, 0], palette="Set2")
-    sns.boxplot(data=df, x="Species", y="SepalWidthCm", ax=axs[1, 1], palette="Set2")
-    plt.show()
+##### Plots Correlation Matrix
 
 def corrMatrixPlot():
     corrMatrix = df.corr()
@@ -110,11 +141,8 @@ def corrMatrixPlot():
     sns.heatmap(corrMatrix, annot = True, ax = ax, cmap="coolwarm")
     plt.show()
 
-###### Function calls
-#summaryFile()
-#plotHistograms()
-#plotScatterplots()
-#pairPlots()
-#plotBoxPlots()
-corrMatrixPlot()
+
+if __name__ == "__main__":
+    main()
+
 
